c46abae9f3160f1f304287d3715273d17fc42865
Soares314 <105524605+Soares314@users.noreply.github.com>
2024-06-24 16:33:50 -0300

    Update c_neddleman.c

--- c_neddleman.c
+++ c_neddleman.c
@@ -4,12 +4,12 @@
 #include <time.h>
 #define MATCH 1;
 #define MISMATCH -1;
-#define GAP 0;
+#define GAP -1;
 
 void nwa(char*, char*);
 
 int main(void) {
-  nwa("CGCTATAT", "TATACTA");
+  nwa("GTCGACGCA", "GTCGACGCA");
   return 0;
 }
 
@@ -17,14 +17,20 @@ void nwa(char* seq1, char* seq2){
   // INICIALIZACAO DA MATRIZ
   clock_t ini = clock();
   int **mat = malloc((strlen(seq1)+1) * sizeof(int*));
+  int **mat_dir = malloc((strlen(seq1)+1) * sizeof(int*));
   for(int i = 0; i<=strlen(seq1)+1; i++){
     mat[i] = malloc((strlen(seq2)+1) * sizeof(int));
+    mat_dir[i] = malloc((strlen(seq2)+1) * sizeof(int*));
   }
+  
   for (int i = 0; i <=strlen(seq1)+1; i++) {
     mat[i][0] = i*GAP;
+    mat_dir[i][0] = 1;
   }
+  
   for(int i = 1; i <=strlen(seq2)+1; i++){
     mat[0][i] = i*GAP;
+    mat_dir[0][i] = 2;
   }
   //PREENCHIMENTO DA MATRIZ
   for(int i = 1; i<=strlen(seq1); i++){
@@ -33,40 +39,46 @@ void nwa(char* seq1, char* seq2){
       int y = 0;
       int z = 0;
       if(seq1[i-1] == seq2[j-1]){
-        x = mat[j-1][i-1] + MATCH; 
+        x = mat[i-1][j-1] + MATCH;
       }else{
-        x = mat[j-1][i-1] + MISMATCH;
+        x = mat[i-1][j-1] + MISMATCH;
       }
-      y = mat[j-1][i] + GAP;
-      z = mat[j][i-1] + GAP;
+      y = mat[i-1][j] + GAP;
+      z = mat[i][j-1] + GAP;
       if(x >= y && x >= z){
-        mat[j][i] = x;
+        mat[i][j] = x;
+        mat_dir[i][j] = 3;
       }else if(y >= x && y >= z){
-        mat[j][i] = mat[j-1][i];
+        mat[i][j] = y;
+        mat_dir[i][j] = 1;
       }else if(z > x && z > y){
-        mat[j][i] = mat[j][i-1];
+        mat[i][j] = z;
+        mat_dir[i][j] = 2;
       }
+    }
+  }
+
+  for(int i = 0; i<=strlen(seq1) + 1; i++){
+    printf("\n");
+    for(int j = 0; j<=strlen(seq2); j++){
+      printf("%2d ", mat_dir[i][j]);
     }
   }
   //ALINHAMENTO
   int col = strlen(seq1);
   int lin = strlen(seq2);
   int gaps = 0;
-  int score = 0;
-  while(col != 0 && lin != 0){
-    if(mat[lin-1][col] > mat[lin-1][col-1] && mat[lin-1][col] > mat[lin][col-1]){
-      lin--;
-      gaps++;
-    }else if(mat[lin][col-1] > mat[lin-1][col-1] && mat[lin][col-1] > mat[lin][col-1]){
+  int score = mat[lin][col];
+  while(col != 0 || lin != 0){
+    if(mat_dir[lin][col] == 2){
       col--;
       gaps++;
-    }else if(mat[lin-1][col-1] < mat[lin][col]){
-      score++;
-      col--;
+    }else if(mat_dir[lin][col] == 1){
       lin--;
+      gaps++;
     }else{
       col--;
-      gaps++;
+      lin--;
     }
   }
   printf("\ntem_de_exec: %f", ((double)clock() - ini) / CLOCKS_PER_SEC);
