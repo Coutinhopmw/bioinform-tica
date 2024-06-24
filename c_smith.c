898b027d44f0b6cf8b56b53aa9e2525597ae880b
Soares314 <105524605+Soares314@users.noreply.github.com>
2024-06-24 17:50:53 -0300

    Update c_smith.c

--- c_smith.c
+++ c_smith.c
@@ -4,12 +4,12 @@
 #include <time.h>
 #define MATCH 1;
 #define MISMATCH -1;
-#define GAP 0;
+#define GAP -1;
 
 void swa(char*, char*);
 
 int main(void) {
-  swa("CGCTATAT", "TATACTA ");
+  swa("AATCG", "AACG");
   return 0;
 }
 
@@ -17,14 +17,20 @@ void swa(char* seq1, char* seq2){
   // INICIALIZACAO DA MATRIZ
   clock_t ini = clock();
   int **mat = malloc((strlen(seq1)+1) * sizeof(int*));
+  int **mat_dir = malloc((strlen(seq1)+1) * sizeof(int*));
   for(int i = 0; i<=strlen(seq1)+1; i++){
     mat[i] = malloc((strlen(seq2)+1) * sizeof(int));
+    mat_dir[i] = malloc((strlen(seq2)+1) * sizeof(int*));
   }
+
   for (int i = 0; i <=strlen(seq1)+1; i++) {
     mat[i][0] = 0;
+    mat_dir[i][0] = 0;
   }
+
   for(int i = 1; i <=strlen(seq2)+1; i++){
     mat[0][i] = 0;
+    mat_dir[0][i] = 0;
   }
   //PREENCHIMENTO DA MATRIZ
   int greater = 0;
@@ -36,51 +42,58 @@ void swa(char* seq1, char* seq2){
       int y = 0;
       int z = 0;
       if(seq1[i-1] == seq2[j-1]){
-        x = mat[j-1][i-1] + MATCH; 
+        x = mat[i-1][j-1] + MATCH;
       }else{
-        x = mat[j-1][i-1] + MISMATCH;
+        x = mat[i-1][j-1] + MISMATCH;
       }
-      y = mat[j-1][i] + GAP;
-      z = mat[j][i-1] + GAP;
+      y = mat[i-1][j] + GAP;
+      z = mat[i][j-1] + GAP;
       if(x >= y && x >= z && x>=0){
-        mat[j][i] = x;
+        mat[i][j] = x;
+        mat_dir[i][j] = 3;
       }else if(y >= x && y >= z && y>=0){
-        mat[j][i] = mat[j-1][i];
+        mat[i][j] = y;
+        mat_dir[i][j] = 1;
       }else if(z > x && z > y && z>=0){
-        mat[j][i] = mat[j][i-1];
+        mat[i][j] = z;
+        mat_dir[i][j] = 2;
       }else{
-        mat[j][i] = 0;
+        mat[i][j] = 0;
+        mat_dir[i][j] = 0;
       }
-      if(mat[j][i]>greater){
-        greater = mat[j][i];
-        greater_j = j;
+      if(mat[i][j]>greater){
+        greater = mat[i][j];
         greater_i = i;
+        greater_j = j;
       }
     }
   }
+
+  for(int i = 0; i<=strlen(seq1); i++){
+    printf("\n");
+    for(int j = 0; j<=strlen(seq2); j++){
+      printf("%2d ", mat[i][j]);
+    }
+  }
   //ALINHAMENTO
-  int col = greater_i;
-  int lin = greater_j;
+  int col = greater_j;
+  int lin = greater_i;
   int gaps = 0;
-  int score = 0;
+  int score = mat[greater_i][greater_j];
   while(mat[lin][col] != 0){
-    if(mat[lin-1][col] > mat[lin-1][col-1] && mat[lin-1][col] > mat[lin][col-1]){
-      lin--;
-      gaps++;
-    }else if(mat[lin][col-1] > mat[lin-1][col-1] && mat[lin][col-1] > mat[lin][col-1]){
+    if(mat_dir[lin][col] == 2){
       col--;
       gaps++;
-    }else if(mat[lin-1][col-1] < mat[lin][col]){
-      score++;
-      col--;
+    }else if(mat_dir[lin][col] == 1){
       lin--;
+      gaps++;
     }else{
       col--;
-      gaps++;
+      lin--;
     }
   }
   printf("\ntem_de_exec: %f", ((double)clock() - ini) / CLOCKS_PER_SEC);
   printf("\nscore: %d", score);
   printf("\ngaps: %d", gaps);
-  printf("\nlin_de_cod: 71");
+  printf("\nlin_de_cod: 61\n");
 }
