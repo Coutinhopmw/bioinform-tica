186b223b94d911312ff12a8b4ff72754913fdf13
Jesse Silva <jesse1eliseu@gmail.com>
2024-06-27 04:51:03 -0300

    Biofinfo.zip

--- Bioinfo/target/classes/codes/cs/SW/obj/Debug/net8.0/SW.AssemblyInfo.cs
+++ Bioinfo/target/classes/codes/cs/SW/obj/Debug/net8.0/SW.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1244b0a2f58621dc183995cc3e17946784c1aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
